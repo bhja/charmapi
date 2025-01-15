e4890117c84f74d019aaad81b02f110795809266
Bhagawati <bh.ja.699@gmail.com>
2025-01-15 14:23:04 +0530

    first commit

---
+++ charmapi.py
@@ -0,0 +1,33 @@
+import json
+
+import requests
+import os
+print("Running Charm api calls")
+refresh_token = os.getenv('CHARM_API_REFRESH_TOKEN')
+data = {'refreshtoken':f'{refresh_token}'}
+r= requests.post(url="https://api.chartmetric.com/api/token",data=data)
+response=r.json()
+
+
+access_token=response['token']
+print("Retrieved the token")
+headers = {
+    'Authorization' : f'Bearer {access_token}'
+}
+
+print("fetch the spotify_monthly_listeners")
+
+
+spotify_artist_id='0du5cEVh5yTK9QJze8zA0C'
+print("******Proceed to fetch the details of the artist views for spotify by city ")
+print("fetch the charm artist id for a spotify artist Bruno Mars", f'{spotify_artist_id}' )
+r=requests.get(url=f"https://api.chartmetric.com/api/artist/spotify/{spotify_artist_id}/get-ids",headers=headers)
+print(r.json())
+
+
+print("Fetching the metrics by day for an artist")
+r=requests.get(url="https://api.chartmetric.com/api/artist/2975/where-people-listen",headers=headers)
+response = r.json()
+print(json.dumps(response,indent=4))
+
+
